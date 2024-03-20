5c8597b449fab7e176ead86821e2c331571411b4
viraavlll <153546280+viraavlll@users.noreply.github.com>
2024-03-20 12:24:00 +0200

    Update ConsoleApplication8.cpp

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 using namespace std;
-void function1(int n, int& a, int& b) {
-    if (n > 7) {
-       a = n / 5;
+void function1(int n, int& a, int& b) { //Функція з параметрами-вказівниками, яка нічого не повертає
+    if (n > 7) { //Перевірка умови
+       a = n / 5; //Параметри функції
        b = (n % 5) / 3;
     }
     else { 
@@ -12,8 +12,8 @@ void function1(int n, int& a, int& b) {
     }
 }
 
-template <class T> void function2(T n, T& a, T& b) {
-    if (n > 7) {
+template <class T> void function2(T n, T& a, T& b) { // Шаблон функції
+    if (n > 7) { //Перевірка умови
         a = n / 5;
         b = (n % 5) / 3;
     }
@@ -25,17 +25,17 @@ template <class T> void function2(T n, T& a, T& b) {
 
 int main() {
     setlocale( LC_CTYPE, "ukr" );
-    int n;
-    int a, b;
+    int n; .//Ініціалізація змінної n
+    int a, b; //Ініціалізація змінної a i b
 
-    cout << "Введiть число n: ";
-    cin >> n;
+    cout << "Введiть число n: "; 
+    cin >> n; //Введення з клавіатури числа n
 
-    function1(n, a, b);
-    function2 <int>(n, a, b);
+    function1(n, a, b); //Оголошення функції 1
+    function2 <int>(n, a, b); //Оголошення функції 2
 
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
+    cout << "a = " << a << endl; //Виведення результату a
+    cout << "b = " << b << endl; //Виведення результату b
     
     return 0;
 }
